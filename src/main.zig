7903f45af695ed6e8628dec4a3fa96ab9a2bed63
Louis Betsch <louisbetsch.b@gmail.com>
2024-09-16 19:34:21 +0200

    added parsing methods for different kdfs

--- src/main.zig
+++ src/main.zig
@@ -1,13 +1,63 @@
 const std = @import("std");
 
+// TODO: Put this in a separate file
+
+const AES256 = [_]u8{ 
+    0x31, 0xC1, 0xF2, 0xE6, 0xBF, 0x71, 0x43, 0x50, 0xBE, 0x58, 0x05, 0x21, 0x6A, 0xFC, 0x5A, 0xF 
+};
+
+const ChaCha2 = [_]u8{
+    0xD6, 0x03, 0x8A, 0x2B, 0x8B, 0x6F, 0x4C, 0xB5, 0xA5, 0x24, 0x33, 0x9A, 0x31, 0xDB, 0xB5, 0x9A
+};
+
+const AESKDF = [_]u8{
+    0xC9, 0xD9, 0xF3, 0x9A, 0x62, 0x8A, 0x44, 0x60, 0xBF, 0x74, 0x0D, 0x08, 0xC1, 0x8A, 0x4F, 0xEA
+};
+
+const Argon2d = [_]u8{
+    0xEF, 0x63, 0x6D, 0xDF, 0x8C, 0x29, 0x44, 0x4B, 0x91, 0xF7, 0xA9, 0xA4, 0x03, 0xE3, 0x0A, 0x0C
+};
+
+const Argon2id = [_]u8{
+    0x9E, 0x29, 0x8B, 0x19, 0x56, 0xDB, 0x47, 0x73, 0xB2, 0x3D, 0xFC, 0x3E, 0xC6, 0xF0, 0xA1, 0xE6
+};
+
 const Error = error{
     CorruptedSignature,
     CorruptedHeader,
+    UnsupportedKDF,
+    UnsupportedKDFVersion,
+    UnsupportedVersion
 };
 
-const AESKdfParams = struct { uuid: ?[]u8, s: ?[]u8, r: ?u64 };
+const AESKdfParams = struct { 
+    salt: ?[]u8, 
+    rounds: ?u64 
+};
+
+const Argon2Params = struct { 
+    isHybrid: bool,
+    version: ?u32,
+    salt: ?[]u8,
+    it: ?u64,
+    mem: ?u64,
+    par: ?u32
+};
+
+const KdfParams = union(enum) {
+    aesKdf: AESKdfParams,
+    argon2: Argon2Params,
+};
+
 const CustomData = struct {};
-const Header = struct { encAlgo: ?[]u8, compAlgo: ?u32, salt: ?[]u8, nonce: ?[]u8, kdfParams: ?AESKdfParams, customData: ?CustomData };
+const Header = struct { 
+    encAlgo: ?[]u8, 
+    compAlgo: ?u32, 
+    salt: ?[]u8, 
+    nonce: ?[]u8, 
+    kdfParams: ?KdfParams, 
+    customData: ?CustomData 
+};
 
 fn byteSliceToU64(ptr: []u8) u64 {
     const res: u64 = @as(u64, @intCast(ptr[0])) | @as(u64, @intCast(ptr[1])) << @intCast(8) | @as(u64, @intCast(ptr[2])) << @intCast(16) | @as(u64, @intCast(ptr[3])) << @intCast(24) | @as(u64, @intCast(ptr[4])) << @intCast(32) | @as(u64, @intCast(ptr[5])) << @intCast(40) | @as(u64, @intCast(ptr[6])) << @intCast(48) | @as(u64, @intCast(ptr[7])) << @intCast(54);
@@ -26,12 +76,15 @@ fn byteSliceToU16(ptr: []u8) u16 {
     return res;
 }
 
-fn parseKdfParams(ptr: []u8) Error!AESKdfParams {
-    var kdfParams = AESKdfParams{ .uuid = null, .s = null, .r = null };
+fn parseKdfParams(ptr: []u8) Error!KdfParams {
+    var kdfParams: ?KdfParams = null;
     var offset: usize = 0;
     const v: u16 = byteSliceToU16(ptr[0..2]);
     offset += 2;
-    std.debug.print("version: {x}\n", .{v});
+
+    if (v != 0x100) {
+        return Error.UnsupportedKDFVersion;
+    }
 
     while (true) {
         const kind = ptr[offset];
@@ -39,7 +92,9 @@ fn parseKdfParams(ptr: []u8) Error!AESKdfParams {
 
         switch (kind) {
             0x00 => {
-                return kdfParams;
+                // NOTE: This is safe due to the null check down below. If the second branch
+                // gets executed first this check down below is performed and an error is returned.
+                return kdfParams.?;
             },
             else => {
                 const nameSize = byteSliceToU32(ptr[offset .. offset + 4]);
@@ -52,13 +107,58 @@ fn parseKdfParams(ptr: []u8) Error!AESKdfParams {
                 offset += valueSize;
 
                 if (std.mem.eql(u8, name, "$UUID")) {
-                    kdfParams.uuid = value;
-                } else if (std.mem.eql(u8, name, "R")) {
-                    kdfParams.r = byteSliceToU64(value);
-                } else if (std.mem.eql(u8, name, "S")) {
-                    kdfParams.s = value;
+                    if (std.mem.eql(u8, value, &AESKDF)) {
+                        kdfParams = KdfParams{ 
+                            .aesKdf = AESKdfParams{
+                                .salt = null,
+                                .rounds = null
+                            }
+                        };
+                    } else if (std.mem.eql(u8, value, &Argon2d) or std.mem.eql(u8, value, &Argon2id)) {
+                        kdfParams = KdfParams{ 
+                            .argon2 = Argon2Params{ 
+                                .isHybrid = std.mem.eql(u8, value, &Argon2id),
+                                .version = null,
+                                .salt = null,
+                                .it = null,
+                                .mem = null,
+                                .par = null,
+                            }
+                        };
+                    } else {
+                        return Error.UnsupportedKDF;
+                    }
                 } else {
-                    return Error.CorruptedHeader;
+                    if (kdfParams == null) {
+                        return Error.CorruptedHeader;
+                    }
+
+                    switch (kdfParams.?) {
+                        .aesKdf => |*params| {
+                            if (std.mem.eql(u8, name, "R")) {
+                                params.*.rounds = byteSliceToU64(value);
+                            } else if (std.mem.eql(u8, name, "S")) {
+                                params.*.salt = value;
+                            } else {
+                                return Error.CorruptedHeader;
+                            }
+                        },
+                        .argon2 => |*params| {
+                            if (std.mem.eql(u8, name, "V")) {
+                                params.*.version = byteSliceToU32(value);
+                            } else if (std.mem.eql(u8, name, "S")) {
+                                params.*.salt = value;
+                            } else if (std.mem.eql(u8, name, "I")) {
+                                params.*.it = byteSliceToU64(value);
+                            } else if (std.mem.eql(u8, name, "M")) {
+                                params.*.mem = byteSliceToU64(value);
+                            } else if (std.mem.eql(u8, name, "P")) {
+                                params.*.par = byteSliceToU32(value);
+                            } else {
+                                return Error.CorruptedHeader;
+                            }
+                        }
+                    }
                 }
             },
         }
@@ -67,10 +167,7 @@ fn parseKdfParams(ptr: []u8) Error!AESKdfParams {
     return Error.CorruptedHeader;
 }
 
-fn parseCustomData(ptr: []u8) Error!CustomData {
-    const v: u16 = byteSliceToU16(ptr[0..2]);
-    std.debug.print("version: {x}", .{v});
-
+fn parseCustomData(_: []u8) Error!CustomData {
     return Error.CorruptedHeader;
 }
 
@@ -149,10 +246,23 @@ pub fn main() !void {
     }
 
     const version = sig[2];
+
+    // TODO: add proper version checking
     std.debug.print("Version: {x}\n", .{version});
 
     const header = try parseHeader(ptr[12..]);
-    std.debug.print("header: {}", .{header});
+
+    switch (header.kdfParams.?) {
+        .aesKdf => |*params| {
+            std.debug.print("aes-kdf params: {}", .{params.*});
+        },
+        .argon2 => |*params| {
+            std.crypto.argon2.kdf();
+            std.debug.print("argon params: {}", .{params.*});
+        }
+    }
+
+    std.debug.print("header: {}\n", .{header});
 }
 
 test "simple test" {}
